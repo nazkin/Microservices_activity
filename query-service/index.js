ebc15ba183e5e2bc99dfd3e06b5655ce21cc654f
Naz Kin <nazkin@pop-os.localdomain>
2021-04-20 18:09:27 -0400

    Finishing off query service

--- query-service/index.js
+++ query-service/index.js
@@ -8,12 +8,31 @@ app.use(cors());
 app.use(express.json());                                //Parsing JSON bodies
 app.use(express.urlencoded({ extended: true }));        //Parsing URL-encoded bodies
 
-app.get('/posts', (req,res) => {
+const postsInfo = {}
 
+app.get('/posts', (req,res) => {
+    res.json({
+        posts: postsInfo
+    });
 });
 
 app.post('/events', (req,res) => {
+    const { type, data } = req.body;
 
+    if(type === 'postCreate'){
+        const { id, title } = data;
+        postsInfo[id] = { id, title, comments: [] };
+
+    }
+    if(type === 'commentCreate'){
+        const { id, comment, postId} = data;
+        postsInfo[postId].comments.push({ id, comment })
+    }
+    console.log(postsInfo);
+    res.json({
+        status: 'OK',
+        message: `${type} event has been successfully processed`
+    })
 });
 
 app.listen(port, () => {
